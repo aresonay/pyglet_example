30a2e416fe30b61c4c104adf95e42d49576e0fb5
Ángel Serón <43752039+GreyGandalf@users.noreply.github.com>
2022-12-24 20:18:11 +0100

    Separated logic and implemented platforms to make colisions in the next version

--- main.py
+++ main.py
@@ -1,93 +1,70 @@
 import pyglet 
 from pyglet.window import key 
+from player import Player
+from platform_1 import Platform
 
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+PLAT_WIDTH = 30
 
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Pyglet Experiment", resizable=False ,fullscreen=False)
 pyglet.gl.glClearColor(*(255,255,255,255))
 
 
 '''Position - X, Position - Y, ObjectWidth, ObjectHeight, Color'''
-rect01 = pyglet.shapes.Rectangle(window.width//2 - 50, window.height // 8 - 10, 20, 20, color=(0,0,0))
+player = Player(window, window.width//2 - 50, window.height // 8 - 10)
 
-#Flags 
-rect_x_move = 'IDLE'
-rect_y_move = 'IDLE'
-rect_left = False 
-rect_right = False
-rect_up = False 
-rect_down = False 
-rect_speed = 5
+
+platforms = [
+    Platform(0,0, WINDOW_WIDTH, PLAT_WIDTH),
+    Platform(0,0, PLAT_WIDTH, WINDOW_WIDTH),
+    Platform(0, WINDOW_HEIGHT-PLAT_WIDTH, WINDOW_WIDTH, PLAT_WIDTH),
+    Platform(WINDOW_WIDTH-PLAT_WIDTH,0, PLAT_WIDTH, WINDOW_HEIGHT)
+]
 
 
 
 
 @window.event
 def on_draw():
-    global rect_x_move, rect_left, rect_right, rect_speed, rect_down, rect_up,rect_y_move
     window.clear()
-    rect01.draw()
+    player.draw()
+    for p in platforms:
+        p.draw()
 
 @window.event
 def on_key_press(symbol, modifiers):
-    global rect_x_move, rect_left, rect_right, rect_speed, rect_down, rect_up,rect_y_move
-    if symbol == key.A:
-        rect_left = True
+    global player 
+    if symbol == key.LEFT:
+        player.go_left = True
         
-    if symbol == key.D:
-        rect_right = True
+    if symbol == key.RIGHT:
+        player.go_right = True
 
-    if symbol == key.W:
-        rect_up = True
+    if symbol == key.UP:
+        player.go_up = True
         
-    if symbol == key.S:
-        rect_down = True
+    if symbol == key.DOWN:
+        player.go_down = True
 
 
 @window.event 
 def on_key_release(symbol, modifiers):
-    global rect_x_move, rect_left, rect_right, rect_speed, rect_down, rect_up,rect_y_move
-    if symbol == key.A:
-        rect_left = False
-    if symbol == key.D:
-        rect_right = False
-    if symbol == key.W:
-        rect_up = False    
-    if symbol == key.S:
-        rect_down = False
+    global player
+    if symbol == key.LEFT:
+        player.go_left = False
+    if symbol == key.RIGHT:
+        player.go_right = False
+    if symbol == key.UP:
+        player.go_up = False    
+    if symbol == key.DOWN:
+        player.go_down = False
 
 def update(dt):
-    global rect_x_move, rect_left, rect_right, rect_speed, rect_down, rect_up,rect_y_move
-    if (rect_left and rect_right) or (not rect_left and not rect_right):
-        rect_x_move = 'IDLE'
-    else: 
-        if rect_left:
-            rect_x_move = 'LEFT'
-        if rect_right:
-            rect_x_move = 'RIGHT'
-
-    if (rect_up and rect_down) or (not rect_up and not rect_down):
-        rect_y_move = 'IDLE'
-    else:
-        if rect_up:
-            rect_y_move = 'UP'
-        if rect_down:
-            rect_y_move = 'DOWN'
-    
-    if rect_x_move == 'LEFT': 
-        rect01.x -= rect_speed    
-    
-    if rect_x_move == 'RIGHT':
-        rect01.x += rect_speed
-    
-    if rect_y_move == 'UP':
-        rect01.y += rect_speed
-    
-    if rect_y_move == 'DOWN':
-        rect01.y -= rect_speed
-
+    global player
+    player.update(dt)
+    player.move()
 
 
 
---
+++ platform_1.py
@@ -0,0 +1,10 @@
+import pyglet 
+
+class Platform():
+    def __init__(self, x, y, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.rect = pyglet.shapes.Rectangle(x, y, self.width, self.height, color=(80,80,80))
+
+    def draw(self):
+        self.rect.draw()
